2a9717aaf36b9fb6ed73132ccf69c3ff58df12e9
Ubuntu <ubuntu@ip-172-31-67-29.ec2.internal>
2020-01-11 18:56:41 +0000

    experiment runner

---
+++ DBM/runexperiments.sh
@@ -0,0 +1,6 @@
+python3 main.py --experiment cifar-s_baseline --experiment_name e1 --random_seed 1
+# python3 main.py --experiment cifar-s_sampling --experiment_name e1 --random_seed 1 > cifar-s_sampling_output.txt
+# python3 main.py --experiment cifar-s_domain_discriminative --experiment_name e1 --random_seed 1 > cifar-s_domain_discrimative_output.txt
+# python3 main.py --experiment cifar-s_domain_independent --experiment_name e1 --random_seed 1 > cifar-s_domain_independent_output.txt 
+# python3 main.py --experiment cifar-s_uniconf_adv --experiment_name e1 --random_seed 1 > cifar-s_uniconf_adv_output.txt
+# python3 main.py --experiment cifar-s_gradproj_adv --experiment_name e1 --random_seed 1 > cifar-s_gradproj_adv.txt
