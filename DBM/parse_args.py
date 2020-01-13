e9a858dfd989873d93c71fddd5ddd7f744223c6f
Ubuntu <ubuntu@ip-172-31-67-29.ec2.internal>
2020-01-13 01:51:23 +0000

    record for cifar_s-baseline

--- DBM/parse_args.py
+++ DBM/parse_args.py
@@ -69,7 +69,7 @@ def create_exerpiment_setting(opt):
         opt['device'] = torch.device('cuda' if opt['cuda'] else 'cpu')
         opt['print_freq'] = 50
         opt['batch_size'] = 128
-        opt['total_epochs'] = 10
+        opt['total_epochs'] = 200
         opt['save_folder'] = os.path.join('record/'+opt['experiment'], 
                                           opt['experiment_name'])
         utils.creat_folder(opt['save_folder'])
@@ -222,7 +222,7 @@ def create_exerpiment_setting(opt):
         
     elif opt['experiment'] == 'cifar-s_gradproj_adv':
         opt['output_dim'] = 10
-        opt['total_epochs'] = 10
+        opt['total_epochs'] = 200
         opt['training_ratio'] = 3
         opt['alpha'] = 1.
         
